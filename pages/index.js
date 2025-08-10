98ac657639cf063e04c7af7397908f6c6ba762bc
melianidjar29-cloud <melianidjar29@gmail.com>
2025-08-10 01:22:12 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,9 @@ export default function Home() {
     <main style={{padding: 24, fontFamily: 'sans-serif'}}>
       <h1>Nartex App</h1>
       <p>Proyecto base funcionando.</p>
-      <a href="/auth">Ir a Login/Registro</a>
-      <a href="/nuevo-pedido">Crear nuevo pedido</a>
+      <a href="/auth">Iniciar sesión</b>
+      <a href="/auth">Registrarse</b>
+      <a href="/nuevo-pedido">Crear nuevo pedido</b>
 
     </main>
   )
