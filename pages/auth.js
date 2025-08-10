7133789a45f45bfdb084c6f48fe41f74a521fd5e
melianidjar29-cloud <melianidjar29@gmail.com>
2025-08-10 00:00:03 -0300

    Delete pages/auth.js

--- pages/auth.js
+++
@@ -1,8 +0,0 @@
-export default function Auth() {
-  return (
-    <main style={{padding: 24, fontFamily: 'sans-serif'}}>
-      <h2>Login / Registro</h2>
-      <p>Acá vamos a conectar Supabase.</p>
-    </main>
-  )
-}
